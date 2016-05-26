b460019643f047dc7cbc0a2ae45ba4113aacfe95
JaeKwanKim <jk79thor@icloud.com>
2016-05-26 14:25:53 +0900

    Spring Boot add Project

---
+++ src/main/java/kr/kakao/lastExam/ApplicationContextForProject.java
@@ -0,0 +1,16 @@
+package kr.kakao.lastExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by JKKim on 2016. 5. 26..
+ */
+@SpringBootApplication
+public class ApplicationContextForProject {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ApplicationContextForProject.class, args);
+    }
+
+}
