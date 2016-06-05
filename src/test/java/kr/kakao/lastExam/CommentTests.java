dd34836e743e6205aed6f31e85f71da6e6fe1fc2
JaeKwanKim <jk79thor@icloud.com>
2016-06-05 13:41:02 +0900

    heroku test

--- src/test/java/kr/kakao/lastExam/CommentTests.java
+++ src/test/java/kr/kakao/lastExam/CommentTests.java
@@ -1,61 +1,60 @@
-package kr.kakao.lastExam;
-
-import kr.kakao.lastExam.dao.CommentRepository;
-import kr.kakao.lastExam.model.Comment;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = { ApplicationContextForProject.class})
-@WebAppConfiguration
-@Transactional
-public class CommentTests {
-	@Autowired
-	private CommentRepository commentRepository;
-	private List<Comment> commentList;
-	@Before
-	public void init() {
-		commentList = new ArrayList<>();
-		Comment comment1 = new Comment("aaaa", "첫번째 테스트 댓글입니다.");
-		Comment comment2 = new Comment("bbbb", "두번째 테스트 댓글입니다.");
-		Comment comment3 = new Comment("cccc", "세번째 테스트 댓글입니다.");
-		Comment comment4 = new Comment("dddd", "네번째 테스트 댓글입니다.");
-		Comment comment5 = new Comment("eeee", "다섯번째 테스트 댓글입니다.");
-		Comment comment6 = new Comment("ffff", "여섯번째 테스트 댓글입니다.");
-		Comment comment7 = new Comment("gggg", "일곱번째 테스트 댓글입니다.");
-		Comment comment8 = new Comment("hhhh", "여덟번째 테스트 댓글입니다.");
-		Comment comment9 = new Comment("jjjj", "아홉번째 테스트 댓글입니다.");
-		Comment comment10 = new Comment("kkkk", "열번째 테스트 댓글입니다.");
-		Comment comment11 = new Comment("qqqq", "열한번째 테스트 댓글입니다.");
-		commentList.add(comment1);
-		commentList.add(comment2);
-		commentList.add(comment3);
-		commentList.add(comment4);
-		commentList.add(comment5);
-		commentList.add(comment6);
-		commentList.add(comment7);
-		commentList.add(comment8);
-		commentList.add(comment9);
-		commentList.add(comment10);
-		commentList.add(comment11);
-	}
-
-	@After
-	public void tearDown(){
-	}
+//package kr.kakao.lastExam;
+//
+//import kr.kakao.lastExam.dao.CommentRepository;
+//import kr.kakao.lastExam.model.Comment;
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.SpringApplicationConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.Matchers.is;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = { ApplicationContextForProject.class})
+//@WebAppConfiguration
+//@Transactional
+//public class CommentTests {
+//	@Autowired
+//	private CommentRepository commentRepository;
+//	private List<Comment> commentList;
+//	@Before
+//	public void init() {
+//		commentList = new ArrayList<>();
+//		Comment comment1 = new Comment("aaaa", "첫번째 테스트 댓글입니다.");
+//		Comment comment2 = new Comment("bbbb", "두번째 테스트 댓글입니다.");
+//		Comment comment3 = new Comment("cccc", "세번째 테스트 댓글입니다.");
+//		Comment comment4 = new Comment("dddd", "네번째 테스트 댓글입니다.");
+//		Comment comment5 = new Comment("eeee", "다섯번째 테스트 댓글입니다.");
+//		Comment comment6 = new Comment("ffff", "여섯번째 테스트 댓글입니다.");
+//		Comment comment7 = new Comment("gggg", "일곱번째 테스트 댓글입니다.");
+//		Comment comment8 = new Comment("hhhh", "여덟번째 테스트 댓글입니다.");
+//		Comment comment9 = new Comment("jjjj", "아홉번째 테스트 댓글입니다.");
+//		Comment comment10 = new Comment("kkkk", "열번째 테스트 댓글입니다.");
+//		Comment comment11 = new Comment("qqqq", "열한번째 테스트 댓글입니다.");
+//		commentList.add(comment1);
+//		commentList.add(comment2);
+//		commentList.add(comment3);
+//		commentList.add(comment4);
+//		commentList.add(comment5);
+//		commentList.add(comment6);
+//		commentList.add(comment7);
+//		commentList.add(comment8);
+//		commentList.add(comment9);
+//		commentList.add(comment10);
+//		commentList.add(comment11);
+//	}
+//
+//	@After
+//	public void tearDown(){
+//	}
 
 //	@Test
 //	public void commentAdd() {
@@ -73,13 +72,11 @@ public class CommentTests {
 //
 //	}
 //
-	@Test
-	public void commentGet() {
+//	@Test
+//	public void commentGet() {
 //		Comment comment = commentRepository.findOne(3);
-		Comment comment = new Comment();
-		assertTo(comment, comment);
 //		assertTo(comment, commentList.get(0));
-	}
+//	}
 //
 //	@Test(expected = EmptyResultDataAccessException.class)
 //	@Rollback
@@ -98,9 +95,9 @@ public class CommentTests {
 //		assertTo(commentRepository.getOne(1), comment);
 //	}
 
-	private void assertTo(Comment comment, Comment testComment) {
-		assertThat(comment.getWriteId(), is(testComment.getWriteId()));
-		assertThat(comment.getContext(), is(testComment.getContext()));
-	}
-
-}
+//	private void assertTo(Comment comment, Comment testComment) {
+//		assertThat(comment.getWriteId(), is(testComment.getWriteId()));
+//		assertThat(comment.getContext(), is(testComment.getContext()));
+//	}
+//
+//}
